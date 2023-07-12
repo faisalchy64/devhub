fd67a3ebabbdfaf1ddf8dfa9ed93b558782b65af
Faisal Karim <faisalchy64@gmail.com>
2023-07-12 15:37:11 +0600

    update - tags component design

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -17,19 +17,51 @@ export default function Tags() {
             id: 3,
             name: "ai",
         },
+        {
+            id: 4,
+            name: "javascript",
+        },
+        {
+            id: 5,
+            name: "python",
+        },
+        {
+            id: 6,
+            name: "react.js",
+        },
+        {
+            id: 7,
+            name: "node.js",
+        },
+        {
+            id: 8,
+            name: "express.js",
+        },
+        {
+            id: 9,
+            name: "mongodb",
+        },
+        {
+            id: 10,
+            name: "next.js",
+        },
     ];
 
     return (
         <section className="w-4/5 mx-auto my-10">
+            <h1 className="text-2xl md:text-3xl font-semibold text-gray-700 mb-5">
+                Discover more of what matters to you
+            </h1>
+
             <div className="flex flex-wrap items-center gap-3.5 text-sm">
                 {tags.map((tag) => (
                     <span
                         key={tag.id}
                         className={`${
                             select === tag.name
-                                ? "text-white bg-blue-500"
-                                : "text-blue-500 bg-blue-50"
-                        } cursor-pointer px-2.5 py-1.5 rounded-md hover:shadow`}
+                                ? "text-white bg-gray-950"
+                                : "text-gray-700 bg-gray-100"
+                        } cursor-pointer px-2.5 py-1.5 rounded-md shadow`}
                         onClick={() => setSelect(tag.name)}
                     >
                         {tag.name}
