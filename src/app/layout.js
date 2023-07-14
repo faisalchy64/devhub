bdd85d10d66626e9c5b640c236a71271814fb944
Faisal Karim <faisalchy64@gmail.com>
2023-07-14 08:22:03 +0600

    create - login page

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,9 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body className={inter.className}>
                 <Navbar />
-                <main className="min-h-screen">{children}</main>
+                <main className="min-h-[calc(100vh-76px)] md:min-h-[calc(100vh-80px)]">
+                    {children}
+                </main>
                 <Footer />
             </body>
         </html>
