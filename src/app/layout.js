c0938b2df937d2b2c7ee7ad41f2296651188c72b
Faisal Karim <faisalchy64@gmail.com>
2023-07-16 20:18:56 +0600

    create - auth_provider component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,12 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <Navbar />
-                <main className="min-h-[calc(100vh-76px)] md:min-h-[calc(100vh-80px)]">
-                    {children}
-                </main>
+                <AuthProvider>
+                    <Navbar />
+                    <main className="min-h-[calc(100vh-76px)] md:min-h-[calc(100vh-80px)]">
+                        {children}
+                    </main>
+                </AuthProvider>
                 <Footer />
             </body>
         </html>
