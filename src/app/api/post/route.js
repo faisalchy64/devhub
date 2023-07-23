1798646dde1c0797b6285b1e06813d31423ab1cf
Faisal Karim <faisalchy64@gmail.com>
2023-07-23 16:19:48 +0600

    create - post get route on post route handler

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -2,6 +2,16 @@ import { NextResponse } from "next/server";
 import connect from "@/common/db";
 import Post from "@/models/postModel";
 
+export const GET = async (req) => {
+    try {
+        await connect();
+        const posts = await Post.find({});
+        return NextResponse.json(posts);
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+};
+
 export const POST = async (req) => {
     try {
         const data = await req.json();
