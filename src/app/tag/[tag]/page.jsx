9628f113661dc598576e284b5562791d80c01113
Faisal Karim <faisalchy64@gmail.com>
2023-07-27 20:47:17 +0600

    add - get post with tag functionality on tag page

--- src/app/tag/[tag]/page.jsx
+++ src/app/tag/[tag]/page.jsx
@@ -1,6 +1,20 @@
 import Post from "@/components/Post";
 
-export default function page({ params }) {
+const getPosts = async (tag) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/tag/${tag}`, {
+            cache: "no-cache",
+        });
+
+        return response.json();
+    } catch (error) {
+        throw new Error("There was an error.");
+    }
+};
+
+export default async function page({ params }) {
+    const posts = await getPosts(params.tag);
+
     return (
         <section className="w-4/5 mx-auto my-10">
             <h1 className="text-3xl md:text-4xl font-semibold text-gray-700">
@@ -11,13 +25,12 @@ export default function page({ params }) {
             </button>
             <hr />
             <div className="flex flex-col gap-5 my-10">
-                <Post />
-                <hr />
-                <Post />
-                <hr />
-                <Post />
-                <hr />
-                <Post />
+                {posts.map((post) => (
+                    <>
+                        <Post key={post._id} post={post} />
+                        <hr />
+                    </>
+                ))}
             </div>
         </section>
     );
